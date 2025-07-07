804186742caa387b9c3b7d560f92fbefb713640f
EdwinLeonM <leonardo.byu@gmail.com>
2025-07-07 04:45:51 +0200

    Second commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,4 @@
 
-// Mostrar el año actual
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// Mostrar la última fecha de modificación
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
