7b650ca2d92dcc2c95b5bcab20bae0d581b7997a
Dev-leoanrd <e.leonardomatias@gmail.com>
2025-07-20 19:52:00 -0500

    commit 07-Country Page

--- scripts/place.js
+++ scripts/place.js
@@ -4,20 +4,28 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
 
 
-// value static
 const temperature = 29; // °C
-const windSpeed = 6;  // km/h
+const windSpeed = 6;    // km/h
 
-// wind chill
 function calculateWindChill(tempC, speedKmh) {
-  return (13.12 + 0.6215 * tempC - 11.37 * Math.pow(speedKmh, 0.16) + 0.3965 * tempC * Math.pow(speedKmh, 0.16)).toFixed(1);
+  return (
+    13.12 +
+    0.6215 * tempC -
+    11.37 * Math.pow(speedKmh, 0.16) +
+    0.3965 * tempC * Math.pow(speedKmh, 0.16)
+  ).toFixed(1);
 }
 
-const windChillElement = document.getElementById('windChill');
+const windChillElements = document.querySelectorAll(".windChill");
 
+let windChillText = "N/A";
 if (temperature <= 10 && windSpeed > 4.8) {
   const windChill = calculateWindChill(temperature, windSpeed);
-  windChillElement.textContent = `${windChill} °C`;
-} else {
-  windChillElement.textContent = "N/A";
+  windChillText = `${windChill} °C`;
 }
+
+windChillElements.forEach(el => {
+  el.textContent = windChillText;
+});
+
+
